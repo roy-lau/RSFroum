83a425b9e3b1b2fdf5c08bc4827f2424c1f7b1a1
roylau <22829291+roy-lau@users.noreply.github.com>
2022-09-04 19:33:20 +0800

    Update db.js
    加密数据库 uri

--- server/db/mongoose/db.js
+++ server/db/mongoose/db.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose'),
-  options = {
-    user: 'admin',
-    pwd: '123456',
-    host: '139.199.99.154',
-    port: '27017',
-    database: 'RSFroum_test',
-    authSource: 'admin',
-  },
-  uri = `mongodb://139.199.99.154:27017/RSFroum_test`;
-// uri = `mongodb://${options.user}:${options.pwd}@${options.host}:${options.port}/${options.database}?authSource=${options.authSource}`;
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+function decipher(str, key, iv){
+  try {
+    const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
+    return decipher.update(str, 'hex', 'utf8') + decipher.final('utf8');
+  } catch(err) {
+    console.error('解密失败');
+    return err.message || err;
+  }
+};
+
+const uri = decipher('f9285c9a70f94bacc7fcc9a497bd2dabf0dbe9ea3f5c0f6ae6fcd4994714408355e6472720a81b9863049275979a127b','key-roy3.1415926','roylau-3.1415926')+'RSFroum_test?authSource=admin'
 
 mongoose.Promise = global.Promise //需要
 mongoose.connection
